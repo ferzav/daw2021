64335a777fab6ada15761c49e88ecdd9ea3c11c9
ferzav <84945976+ferzav@users.noreply.github.com>
2021-05-27 20:26:05 -0500

    Add files via upload

---
+++ aritmetica.js
@@ -0,0 +1,17 @@
+  function limpiarFormulario() {
+    document.getElementById("miForm").reset();
+  }
+
+
+  var suma = function  (n1, n2){
+
+			var numero1 = parseInt(document.getElementById("numero1").value);
+
+			var numero2 = parseInt(document.getElementById("numero2").value);
+
+			var resultado = numero1 + numero2;
+
+			return resultado; 
+
+		}
+
